6b521bd555b6c64b652ae184ba528f55b540ea2f
shengli <363912809@qq.com>
2018-12-02 22:06:24 +0800

    audio

---
+++ application/admin/lang/zh-cn/audio/audio.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Id'  =>  'ID',
+    'Video_lesson_id'  =>  '难度',
+    'Title'  =>  '歌曲标题',
+    'Timelong'  =>  '时长',
+    'Singer'  =>  '歌手',
+    'Album'  =>  '专辑',
+    'Createtime'  =>  '创建时间'
+];
---
+++ public/assets/js/backend/audio/audio.js
@@ -0,0 +1,55 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'audio/audio/index',
+                    add_url: 'audio/audio/add',
+                    edit_url: 'audio/audio/edit',
+                    del_url: 'audio/audio/del',
+                    multi_url: 'audio/audio/multi',
+                    table: 'audio',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'video_lesson_id', title: __('Video_lesson_id')},
+                        {field: 'title', title: __('Title')},
+                        {field: 'timelong', title: __('Timelong')},
+                        {field: 'singer', title: __('Singer')},
+                        {field: 'album', title: __('Album')},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
