5d2446a31a64da11a134c9c78f6af5cf9494d152
shengli <363912809@qq.com>
2018-12-22 10:57:06 +0800

    public

---
+++ public/assets/js/backend/video/singledance.js
@@ -0,0 +1,55 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            var video_episode_id = $('input[name=video_episode_id]').val();
+
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'video/singledance/index?video_episode_id='+video_episode_id,
+                    add_url: 'video/singledance/add?video_episode_id='+video_episode_id,
+                    edit_url: 'video/singledance/edit',
+                    del_url: 'video/singledance/del',
+                    multi_url: 'video/singledance/multi',
+                    table: 'video_singledance',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'video_episode_id', title: __('Video_episode_id')},
+                        {field: 'title', title: __('Title')},
+                        {field: 'video', title: __('Video'), visible: false},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
